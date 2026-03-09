490ddffa6bb3f63793292ca765b981a2651c1f41
codelabproject315-spec <codelabproject315@gmail.com>
2026-03-09 16:35:26 +0900

    Update PlayerCharacter.jsx

--- src/PlayerCharacter.jsx
+++ src/PlayerCharacter.jsx
@@ -36,7 +36,7 @@ const PlayerCharacter = ({ map, lat, lng, bearing }) => {
           vrmRef.current = vrm;
 
           // モデルのサイズ調整（地図のスケールに合わせる）
-          vrm.scene.scale.set(15, 15, 15);
+          vrm.scene.scale.set(10, 10, 10);
           vrm.scene.rotation.x = Math.PI / 2; // 立たせる
           vrm.scene.rotation.z = -(bearing ?? 0) * (Math.PI / 180); // 向き
           
