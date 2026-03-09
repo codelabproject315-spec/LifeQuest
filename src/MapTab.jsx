d0f1397e9e04a35681544b419ab5202deadc18dd
codelabproject315-spec <codelabproject315@gmail.com>
2026-03-09 15:01:37 +0900

    Update MapTab.jsx

--- src/MapTab.jsx
+++ src/MapTab.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Navigation, X } from 'lucide-react';
 import maplibregl from 'maplibre-gl'; // 👈 ここを追加
 import 'maplibre-gl/dist/maplibre-gl.css'; // 👈 スタイルもインポート
-import PlayerCharacter from './PlayerCharacter'; // 新しいファイルをインポート
+import PlayerCharacter from './PlayerCharacter.jsx'; // 新しいファイルをインポート
 
 const MapTab = ({ quests, userLocation, gpsStatus, mockOffset, setMockOffset, QUEST_LAT, QUEST_LNG }) => {
   const mapRef = useRef(null);
