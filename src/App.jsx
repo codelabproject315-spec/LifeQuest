0eab8e61bb3c5f3cbef7320006093864e2413097
codelabproject315-spec <codelabproject315@gmail.com>
2026-03-09 12:43:51 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1337,7 +1337,7 @@ export default function App() {
           </div>
         )}
         {activeTab === 'map' && (
-          <div style={{ height: '100%' }}>
+          <div style={{ height: 'calc(100% - 96px)' }}>
             <MapTab quests={quests} userLocation={userLocation} gpsStatus={gpsStatus} mockOffset={mockOffset} setMockOffset={setMockOffset} QUEST_LAT={QUEST_LAT} QUEST_LNG={QUEST_LNG} />
           </div>
         )}
